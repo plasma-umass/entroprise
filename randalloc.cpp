0ba7c9c2aa1e471cb162964d22a7e6cbc97fedc1
Michael Steranka <msteranka@umass.edu>
2019-11-09 15:38:12 -0500

    Added dieharder stuff for allocators

---
+++ randalloc.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <unordered_map>
+
+using namespace std;
+
+void *ec_malloc(size_t sz) {
+	void *ptr = malloc(sz);
+	if (ptr == NULL) {
+		perror("error:");
+		exit(EXIT_FAILURE);
+	}
+	else return ptr;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		cerr << "usage: [NOBJS] [SIZE]" << endl;
+		return -1;
+	}
+
+	const int NOBJS = stoi(argv[1]), SZ = stoi(argv[2]);
+	char **objs = new char *[NOBJS];
+
+	for (int i = 0; i < NOBJS; ++i)
+		objs[i] = (char *) ec_malloc(SZ);
+	for (int i = 0; i < NOBJS; ++i)
+		free(objs[i]);
+
+	delete objs[NOBJS];
+
+	srand(time(NULL));
+	int next = 0, r = rand();
+	unordered_multimap<char *, int> counter;
+	unordered_multimap<char *, int>::iterator it;
+
+	for (int i = 0; i < NOBJS; ++i) {
+		char *ptr = (char *) ec_malloc(SZ);
+		it = counter.find(ptr);
+	  	if (it == counter.end()) // If not in map,
+			counter.insert(pair<char *, int>(ptr, ++next)); // Then insert it with unique value
+		else // If in map,
+			counter.insert(pair<char *, int>(ptr, it->second)); // Then duplicate it
+		free(ptr);
+	}
+
+	for (it = counter.begin(); it != counter.end(); ++it) {
+		unsigned int cur = it->second ^ r;
+		fwrite(&cur, sizeof(cur), 1, stdout);
+	}
+
+	return 0;
+}
