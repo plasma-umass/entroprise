bbf2e7754317de2f9ce7a3cc638d40ce963396fe
Michael Steranka <msteranka@umass.edu>
2020-02-10 19:38:30 -0500

    Added HyperLogLog and ran up to 10000000 objs

--- original-entroprise/RUNME.sh
+++ original-entroprise/RUNME.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Runs all allocators for every given object size, number of objects, and number of threads
 
-outspath="out" # Directory for output of each allocator
+outspath="approx" # Directory for output of each allocator
 outs=("dieharder" "freeguard" "guarder" "scudo" "jemalloc" "tcmalloc" "smimalloc") # Names for output files
 libspath="/root/496/lib" # Directory for shared libraries
 libs=("libdiehard.so" "libfreeguard.so" "libguarder.so" "libclang_rt.scudo-x86_64.so" "libjemalloc.so" "libtcmalloc.so.4" "libmimalloc-secure.so") # Names of shared libraries (order of libraries must match names of each in outs)
-sizes=(16 32 64 128 256 512 1024 2048 4096 8192 16384 32768) # Size classes
-nobjs=(100 1000 10000) # Number of objects
+# sizes=(16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536) # Size classes
+sizes=(16 32 64) # Size classes
+nobjs=(100 1000 10000 100000 1000000 10000000) # Number of objects
 nthreads=(1 2 4 8) # Number of threads
 
 run() { # Runs $1 allocator and puts output in $2 file
@@ -21,7 +22,7 @@ run() { # Runs $1 allocator and puts output in $2 file
 			do
 				printf "\t\t%-5d   " $k >> $2
 				# LD_PRELOAD=$1 ./entroprise $k $j $i >> $2
-				./entroprise $k $j $i >> $2
+				./entroprise $k $j $i y >> $2
 			done
 			echo "" >> $2
 		done
--- original-entroprise/entroprise.cpp
+++ original-entroprise/entroprise.cpp
@@ -8,6 +8,8 @@
 #include <map>
 #include <mutex>
 #include <heaplayers>
+#include "hyperloglog.hpp"
+#include "murmur3.h"
 
 using namespace HL;
 using namespace std;
@@ -15,25 +17,41 @@ using namespace std;
 template <typename T>
 class MyAllocator : public STLAllocator<T, FreelistHeap<BumpAlloc<4096, MmapHeap>>> {};
 
-float run(const int OBJECT_SIZE, const int MIN_ALLOC) {
+void fatal() {
+    perror("ERROR:");
+    exit(-1);
+}
+
+void primeHeap(const int OBJECT_SIZE, const int MIN_ALLOC) {
 	char **objs = new char *[MIN_ALLOC];
-	unordered_map<char *, int, hash<char *>, equal_to<char *>, MyAllocator<pair<char * const, int>>> counter;
 
 	for (int i = 0; i < MIN_ALLOC; i++) {
 		objs[i] = (char *) malloc(OBJECT_SIZE);
+        if (objs[i] == NULL) {
+            fatal();
+        }
 	}
   	for (int i = 0; i < MIN_ALLOC; i++) {
 		free(objs[i]);
 	}
+
 	delete [] objs;
+}
+
+float getExactEntropy(const int OBJECT_SIZE, const int MIN_ALLOC) {
+	unordered_map<char *, int, hash<char *>, equal_to<char *>, MyAllocator<pair<char * const, int>>> counter;
+    primeHeap(OBJECT_SIZE, MIN_ALLOC);
 
   	for (int i = 0; i < MIN_ALLOC; i++) {
-	  char *p = (char *) malloc(OBJECT_SIZE);
-	  free(p);
-	  if (counter.find(p) == counter.end()) {
-	    counter[p] = 0;
-	  }
-	  counter[p]++;
+	    char *p = (char *) malloc(OBJECT_SIZE);
+        if (p == NULL) {
+            fatal();
+        }
+	    free(p);
+	    if (counter.find(p) == counter.end()) {
+	      counter[p] = 0;
+	    }
+	    counter[p]++;
   	}
 
   	float entropy = 0.0;
@@ -45,25 +63,48 @@ float run(const int OBJECT_SIZE, const int MIN_ALLOC) {
 	return entropy;
 }
 
+float getBoundedEntropy(const int OBJECT_SIZE, const int MIN_ALLOC) {
+    hll::HyperLogLog hll(16); // Initialize HyperLogLog with 2^16 bit width
+    primeHeap(OBJECT_SIZE, MIN_ALLOC);
+
+    for (int i = 0; i < MIN_ALLOC; i++) {
+	    void *ptr = (void *) malloc(OBJECT_SIZE);
+        if (ptr == NULL) {
+            fatal();
+        }
+	    free(ptr);
+        hll.add((char *) &ptr, sizeof(void *));
+    }
+
+    return log(hll.estimate()) / log(2.0);
+}
+
 int main(int argc, char *argv[]) {
-	if (argc != 4) {
-		cerr << "usage: <OBJECT_SIZE> <MIN_ALLOC> <NTHREADS>" << endl;
+	if (argc != 5) {
+		cerr << "usage: <OBJECT_SIZE> <MIN_ALLOC> <NTHREADS> <BOUND=y/n>" << endl;
 		return -1;
 	}
 
 	const int OBJECT_SIZE = stoi(argv[1]), MIN_ALLOC = stoi(argv[2]), NTHREADS = stoi(argv[3]);
+    bool approx = (argv[4][0] == 'y');
 	future<float> *threads = new future<float>[NTHREADS];
 	float entropy = 1, max = log(MIN_ALLOC / NTHREADS) / log(2.0);
 
-	for (int i = 0; i < NTHREADS; i++) // Run all threads
-		threads[i] = async(run, OBJECT_SIZE, MIN_ALLOC / NTHREADS);
-	for (int i = 0; i < NTHREADS; i++) // Compute average
-		entropy = entropy * threads[i].get();
+    if (approx) { // Bound entropy
+        for (int i = 0; i < NTHREADS; i++) {
+            threads[i] = async(getBoundedEntropy, OBJECT_SIZE, MIN_ALLOC / NTHREADS);
+        }
+    } else { // Get exact entropy
+        for (int i = 0; i < NTHREADS; i++) {
+            threads[i] = async(getExactEntropy, OBJECT_SIZE, MIN_ALLOC / NTHREADS);
+        }
+    }
 
-	entropy = pow(entropy, 1.0 / NTHREADS); // Compute average 
-	cout << fixed;
-	cout << setprecision(4); // For alignment
-	cout << entropy << "   " << max << "   " << entropy * 100.0 / max << "%" << endl;
+	for (int i = 0; i < NTHREADS; i++) { // Compute average
+		entropy *= threads[i].get();
+    }
+	entropy = pow(entropy, 1.0 / NTHREADS);
 
+	cout << fixed << setprecision(4) << entropy << "   " << max << "   " << entropy * 100.0 / max << "%" << endl;
   	return 0;
 }
