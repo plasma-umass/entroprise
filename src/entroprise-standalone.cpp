ee26c9ee243519ae4415cf301fdb917fa5415741
Michael Steranka <msteranka@cs.umass.edu>
2021-04-06 17:29:12 -0400

    Fixed indenting in standalone

--- src/entroprise-standalone.cpp
+++ src/entroprise-standalone.cpp
@@ -14,42 +14,42 @@ class MyAllocator : public HL::STLAllocator<T, HL::FreelistHeap<HL::BumpAlloc<40
 void prime_heap(const int OBJECT_SIZE, const int NUM_ALLOCS) {
     // Dynamically allocated because stack allocation can segfault
     //
-	char **objs = new char *[NUM_ALLOCS];
+    char **objs = new char *[NUM_ALLOCS];
 
-	for (int i = 0; i < NUM_ALLOCS; i++) {
-		objs[i] = (char *) malloc(OBJECT_SIZE);
+    for (int i = 0; i < NUM_ALLOCS; i++) {
+        objs[i] = (char *) malloc(OBJECT_SIZE);
         assert(objs[i]);
-	}
-  	for (int i = 0; i < NUM_ALLOCS; i++) {
-		free(objs[i]);
-	}
-
-	delete[] objs;
+    }
+    for (int i = 0; i < NUM_ALLOCS; i++) {
+        free(objs[i]);
+    }
+    
+    delete[] objs;
 }
 
 double get_exact_entropy(const int OBJECT_SIZE, const int NUM_ALLOCS) {
-	std::unordered_map<char *, int, std::hash<char *>, std::equal_to<char *>, 
+    std::unordered_map<char *, int, std::hash<char *>, std::equal_to<char *>, 
                        MyAllocator<std::pair<char * const, int>>> counter;
     double entropy;
     prime_heap(OBJECT_SIZE, NUM_ALLOCS);
 
-  	for (int i = 0; i < NUM_ALLOCS; i++) {
-	    char *ptr = (char *) malloc(OBJECT_SIZE);
+    for (int i = 0; i < NUM_ALLOCS; i++) {
+        char *ptr = (char *) malloc(OBJECT_SIZE);
         assert(ptr);
-	    if (counter.find(ptr) == counter.end()) {
-	      counter[ptr] = 0;
-	    }
-	    counter[ptr]++;
-	    free(ptr);
-  	}
+        if (counter.find(ptr) == counter.end()) {
+            counter[ptr] = 0;
+        }
+        counter[ptr]++;
+        free(ptr);
+    }
 
-  	entropy = 0;
-  	for (auto it = counter.begin(); it != counter.end(); it++) {
-		char *address = (*it).first;
-  	  	int count = (*it).second;
-  	  	entropy += -log(count / (double) NUM_ALLOCS) / log(2.0) * (count / (double) NUM_ALLOCS);
-  	}
-	return entropy;
+    entropy = 0;
+    for (auto it = counter.begin(); it != counter.end(); it++) {
+        char *address = (*it).first;
+        int count = (*it).second;
+        entropy += -log(count / (double) NUM_ALLOCS) / log(2.0) * (count / (double) NUM_ALLOCS);
+    }
+    return entropy;
 }
 
 double get_approx_entropy(const int OBJECT_SIZE, const int NUM_ALLOCS, const uint8_t BIT_WIDTH) {
@@ -57,32 +57,32 @@ double get_approx_entropy(const int OBJECT_SIZE, const int NUM_ALLOCS, const uin
     //
     hll::HyperLogLog hll(BIT_WIDTH);
     prime_heap(OBJECT_SIZE, NUM_ALLOCS);
-
+    
     for (int i = 0; i < NUM_ALLOCS; i++) {
-	    char *ptr = (char *) malloc(OBJECT_SIZE);
+        char *ptr = (char *) malloc(OBJECT_SIZE);
         assert(ptr);
         hll.add((const char *) &ptr, sizeof(ptr));
-	    free(ptr);
+        free(ptr);
     }
-
+    
     return std::log(hll.estimate()) / log(2.0);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 5) {
-		std::cerr << "usage: <OBJECT_SIZE> <NUM_ALLOCS> <NUM_THREADS> <APPROXIMATE=y/n>" << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	const unsigned int OBJECT_SIZE = std::stoi(argv[1]), 
+    if (argc != 5) {
+        std::cerr << "usage: <OBJECT_SIZE> <NUM_ALLOCS> <NUM_THREADS> <APPROXIMATE=y/n>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    const unsigned int OBJECT_SIZE = std::stoi(argv[1]), 
                        NUM_ALLOCS = std::stoi(argv[2]), 
                        NUM_THREADS = std::stoi(argv[3]);
     const bool IS_APPROX = (argv[4][0] == 'y');
     const uint8_t BIT_WIDTH = 16;
     const double MAX_ENTROPY = std::log(NUM_ALLOCS) / std::log(2.0);
     std::future<double> threads[NUM_THREADS];
-	double entropy;
-
+    double entropy;
+    
     if (IS_APPROX) { // Get approximate entropy using HyperLogLog
         for (int i = 0; i < NUM_THREADS; i++) {
             threads[i] = std::async(get_approx_entropy, OBJECT_SIZE, NUM_ALLOCS, BIT_WIDTH);
@@ -92,15 +92,15 @@ int main(int argc, char *argv[]) {
             threads[i] = std::async(get_exact_entropy, OBJECT_SIZE, NUM_ALLOCS);
         }
     }
-
+    
     entropy = 1;
     for (int i = 0; i < NUM_THREADS; i++) { // Compute geometric average
-		entropy *= threads[i].get();
+        entropy *= threads[i].get();
     }
-	entropy = pow(entropy, 1.0 / NUM_THREADS);
-
-	std::cout << std::fixed << std::setprecision(4) << entropy << "   " << MAX_ENTROPY << "   " << 
+    entropy = pow(entropy, 1.0 / NUM_THREADS);
+    
+    std::cout << std::fixed << std::setprecision(4) << entropy << "   " << MAX_ENTROPY << "   " << 
                  entropy / MAX_ENTROPY << std::endl;
-
-  	return EXIT_SUCCESS;
+                 
+    return EXIT_SUCCESS;
 }
